44dd7431ce3d1084e84e20d77773853b74632803
MarkyJD <marky.dodson@gmail.com>
2021-10-23 16:01:07 +1100

    Add basic components and responsive Layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,65 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 import './App.css';
+import Header from './components/Header/Header';
+
+/* 
+      This is the big picture view of the layout
+
+                                COL
+             --------------------------------------------
+         ROW |                                          |
+             |               Header                     |
+             --------------------------------------------
+             |            |                             |
+             |            |                             |
+             |     Side   |                             |
+         ROW |     Bar    |     Map Section             |
+             |            |                             |
+             |            |                             |
+             |            |                             |
+             |            |                             |
+             |            |                             |
+             --------------------------------------------
+                  COL                 COL
+*/
 
 function App() {
 
-  const [data, setData] = useState(null);
+  // Commented out fetching to work on layout
 
-  useEffect(() => {
-    fetch("/api")
-    .then((res) => res.json())
-    .then((data) => setData(data.message));
-  }, []);
+  // const [data, setData] = useState(null);
+
+  // useEffect(() => {
+  //   fetch("/api")
+  //   .then((res) => res.json())
+  //   .then((data) => setData(data.message));
+  // }, []);
 
   return (
-    <div className="App">
-        {/* Bootstrap classes */}
-        <p className="h1 text-center mt-5">
-          
-          {/* Displays loading... if data has loaded yet */}
-          {!data ? "loading..." : data}
-        </p>
-        
-     
+    
+    // Entire app container
+    <div className="container-fluid min-vh-100 d-flex flex-column">
+      
+      {/* Header row with one col */}
+      <div className="row">
+        <div className="col border bg-primary">
+            <Header />
+        </div>
+      </div>
+
+      {/* 2nd row. Two cols - Sidebar and Map Section */}
+      <div className="row flex-grow-1">
+
+        {/* Bootstrap Responsive resizing */}
+        <div className="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-3 border bg-secondary">
+            Sidebar
+        </div>
+
+        {/* Bootstrap Responsive resizing */}
+        <div className="col-12 col-sm-12 col-md-12 col-lg-8 col-xl-9 border bg-danger">
+            Map
+        </div>
+      </div>
     </div>
   );
 }
