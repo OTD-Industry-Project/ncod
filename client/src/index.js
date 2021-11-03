f2255e82289a274ea8a6109646ccce1e8bdaf738
MarkyJD <54533821+MarkyJD@users.noreply.github.com>
2021-11-03 13:09:23 +1100

    Fix issue with Marker displaying incorrectly

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery';
 import '@popperjs/core';
-import 'leaflet/dist/leaflet.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
