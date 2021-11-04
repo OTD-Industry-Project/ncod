ee01002b2f2266d7735303022dabb955661d29e2
J-Hawes <91867589+J-Hawes@users.noreply.github.com>
2021-11-05 09:27:04 +1100

    Fixed looping issues with overlay

--- client/src/components/MapWrapper/Layers.jsx
+++ client/src/components/MapWrapper/Layers.jsx
@@ -3,8 +3,10 @@ import { TileLayer, LayersControl, Marker, Popup, ZoomControl } from 'react-leaf
 import addressData from "../../data/address.json"
 
 const Layers = () => {
-    const bus = [new Set(addressData.locations.map(buses => buses.statusUpdate))]
-    console.log(bus);
+
+    //Create array with JSON data imported from ./data/address.json//
+
+    const bus = Array.from(addressData.locations.map(buses => buses.statusUpdate));
 
     return (
         <>
@@ -15,17 +17,18 @@ const Layers = () => {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
 
+                {/* Adding each layer for visibility to be toggled on and off as required
+                by looping through the array*/}
+                
+                {addressData.locations.map((buses) => (
+                    
 
+                    <LayersControl.Overlay 
+                    checked name={buses.statusUpdate}
+                    key={buses.statusUpdate}>
 
-                {/* Adding each layer for visibility to be toggled on and off as required.*/}
-
-                <LayersControl.Overlay checked name="On Schedule">
-
-                    {/* Looping through JSON array imported from ./data/address.json to display markers */}
-
-                    {addressData.locations.map(buses => (
                         <Marker
-                            key={buses.addr_name}
+                            eventKey={buses.addr_name}
                             position={[buses.addr_lat, buses.addr_long]}
 
                             /* Using event handlers to access mouseover/out features for hover info
@@ -42,33 +45,10 @@ const Layers = () => {
                                 {buses.addr_suburb}
                             </Popup>
                         </Marker>
-                    ))}
 
-                </LayersControl.Overlay>
-{/* 
-                <LayersControl.Overlay checked name="Delayed">
-                    {addressData.locations.map(buses => (
-                        <Marker
-                            key={buses.addr_name}
-                            position={[buses.addr_lat, buses.addr_long]}
+                    </LayersControl.Overlay>
+                ))}
 
-                             //Using event handlers to access mouseover/out features for hover info
-                            to be defined inside the Popup tags  //
-
-                            eventHandlers={{
-                                mouseover: (event) => event.target.openPopup(),
-                                mouseout: (event) => event.target.closePopup(),
-                            }}
-                        >
-                            <Popup>
-                                <h6>{buses.addr_name}</h6> <br />
-                                {buses.addr_street} <br />
-                                {buses.addr_suburb}
-                            </Popup>
-                        </Marker>
-                    ))}
-                </LayersControl.Overlay>
-                 */}
             </LayersControl>
         </>
     )
