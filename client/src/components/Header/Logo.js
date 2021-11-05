6beb279cd60575957744bdd6acc2bb493eda539e
JamieG22 <jamie_garner@hotmail.com>
2021-11-06 02:37:33 +1100

    small logo adjust and tooltip

--- client/src/components/Header/Logo.js
+++ client/src/components/Header/Logo.js
@@ -5,7 +5,7 @@ import Tooltip from '@mui/material/Tooltip';
 const Logo = () => {
     return (
         <div className="Logo d-flex justify-content-center">
-            <Tooltip disableFocusListener disableTouchListener title="Round & Round">
+            <Tooltip disableFocusListener disableTouchListener title="Round & Round" followCursor="true">
                 <img src={logo} alt="otd logo" height="45"/>
             </Tooltip>
             
