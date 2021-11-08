a9f1d74cfe74135492db8fc70a0f2e3665ee3606
J-Hawes <91867589+J-Hawes@users.noreply.github.com>
2021-11-09 09:32:32 +1100

    pruned package-lock, and added test_data set on layers.jsx

--- client/src/components/MapWrapper/Layers.jsx
+++ client/src/components/MapWrapper/Layers.jsx
@@ -2,43 +2,56 @@ import React from "react";
 import {
     TileLayer,
     LayersControl,
-    Marker,
+    CircleMarker,
     Popup,
     ZoomControl,
 } from "react-leaflet";
-import addressData from "../../data/address.json";
+import test_data from "../../data/TEST_DATA.json"
 
 const Layers = ({ schedule }) => {
-    //Create array with JSON data imported from ./data/address.json//
-
+    
     console.log(schedule);
+    
+    const data = test_data;
 
-    const bus = Array.from(
-        addressData.locations.map((buses) => buses.statusUpdate)
-    );
+    //testing arrays and looping through data to extract something useful for layers control//
+
+    // const bus = Array.from(addressData.locations.map(buses => buses.statusUpdate));
+
+    
+    /*     var busStatus = data.reduce(function (r, a) {
+            r[a.SingleJourney] = r[a.SingleJourney] || [];
+            r[a.SingleJourney].push(a);
+            return r;
+        }, Object.create(null));
+    
+        console.log(busStatus); */
+
 
     return (
         <>
-            <ZoomControl position="topright" />{" "}
-            {/* fully customisable zoom controls*/}
-            <LayersControl position="topright">
-                {" "}
-                {/* layer control panel */}
+            <ZoomControl position="topright" /> {/* fully customisable zoom controls*/}
+            <LayersControl position="topright"> {/* layer control panel */}
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+
                 {/* Adding each layer for visibility to be toggled on and off as required
                 by looping through the array*/}
-                {addressData.locations.map((buses) => (
-                    <LayersControl.Overlay
-                        checked
-                        name={buses.statusUpdate}
-                        key={buses.statusUpdate}
-                    >
-                        <Marker
-                            eventKey={buses.addr_name}
-                            position={[buses.addr_lat, buses.addr_long]}
+
+                <LayersControl.Overlay
+                    checked name={"DriverID: " + data.DriverID + " Driving Bus# " + data.VehicleID} //planning to sort this somehow
+                    key={data.element}>
+
+                    {/* looping over the pickup points, and pltting with a blue circle */}
+
+                    {data.map((buses) => (
+
+                        <CircleMarker
+                            eventKey={buses.VehicleID + buses.DriverID}
+                            center={[buses.PickupPointLatitude, buses.PickupPointLongitude]}
+
                             /* Using event handlers to access mouseover/out features for hover info
                             to be defined inside the Popup tags  */
 
@@ -48,16 +61,46 @@ const Layers = ({ schedule }) => {
                             }}
                         >
                             <Popup>
-                                <h6>{buses.addr_name}</h6> <br />
-                                {buses.addr_street} <br />
-                                {buses.addr_suburb}
+                                <h6>{buses.PickupPoint}</h6> <br />
+                                VehicleID : {buses.VehicleID} <br />
+                                DriverID : {buses.DriverID}
                             </Popup>
-                        </Marker>
-                    </LayersControl.Overlay>
-                ))}
+
+                        </CircleMarker>
+
+                    ))}
+
+                    {/* looping over the destination points, and pltting with a red circle */}
+
+                    {data.map((buses) => (
+
+                        <CircleMarker
+                            eventKey={buses.VehicleID + buses.DriverID}
+                            center={[buses.DestinationLatitude, buses.DestinationLongitude]}
+                            color={'dark'} //default is blue
+
+                            /* Using event handlers to access mouseover/out features for hover info
+                            to be defined inside the Popup tags  */
+
+                            eventHandlers={{
+                                mouseover: (event) => event.target.openPopup(),
+                                mouseout: (event) => event.target.closePopup(),
+                            }}
+                        >
+                            <Popup>
+                                <h6>{buses.Destination}</h6> <br />
+                                VehicleID : {buses.VehicleID} <br />
+                                DriverID : {buses.DriverID}
+                            </Popup>
+
+                        </CircleMarker>
+
+                    ))}
+                </LayersControl.Overlay>
+
             </LayersControl>
         </>
-    );
-};
+    )
+}
 
 export default Layers;
