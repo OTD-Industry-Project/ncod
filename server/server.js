311256d582e0961f044bb997ba589e3ffa32e0c4
J-Hawes <91867589+J-Hawes@users.noreply.github.com>
2021-11-16 12:16:06 +1100

    started building out the server routes

--- server/server.js
+++ server/server.js
@@ -11,15 +11,55 @@ const db = require("./db");
 require("dotenv").config();
 
 // Use environment variable if configured, otherwise use port 3000
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 // Instantiate express app
 const app = express();
 
+app.use(express.json());
+
+
 // ROUTES
-app.get("/api", async (req, res) => {
-    res.json({message: "Hello from Server"});
+
+//Get all address
+app.get("/api/address", async (req, res) => {
+
+    try {
+        const results = await db.query("SELECT * FROM address");
+        console.log(results.rows);
+        res.status(200).json({
+            status: "success",
+            results: results.rows.length,
+            data: {
+                address: results.rows,
+            },
+        });
+    } 
+    catch (err) {
+        console.log(err);
+    }
 });
+
+
+//Get address by ID
+app.get("/api/address/:id", async (req, res) => {
+    try {
+        const results = await db.query(`SELECT * FROM address WHERE addr_id = ${req.params.id}`);
+        console.log(results.rows[0]);
+        res.status(200).json({
+            status: "success",
+            data: {
+                address: results.rows[0],
+            },
+        });
+    } 
+    catch (err) {
+        console.log(err);
+    }
+});
+
+
+
 
 // Run Server
 app.listen(PORT, () => {
