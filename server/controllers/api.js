7740aa10a6829360ba10b1d83ca2ce693fb51de7
J-Hawes <91867589+J-Hawes@users.noreply.github.com>
2021-11-17 19:39:52 +1100

    added drivers controller

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -33,6 +33,39 @@ const getAddressID = async(req, res) => {
     }
 };
 
+const getAllDrivers = async(req, res) => {
+    try {
+        const results = await db.query("SELECT * FROM drivers");
+        console.log(results.rows);
+        res.status(200).json({
+            status: "success",
+            results: results.rows.length,
+            data: {
+                drivers: results.rows,
+            },
+        });
+    } 
+    catch (err) {
+        console.log(err);
+    }
+};
+
+const getDriverID = async(req, res) => {
+    try {
+        const results = await db.query("SELECT * FROM drivers WHERE driver_id = $1", [req.params.id]);
+        console.log(results.rows[0]);
+        res.status(200).json({
+            status: "success",
+            data: {
+                driver: results.rows[0],
+            },
+        });
+    } 
+    catch (err) {
+        console.log(err);
+    }
+};
+
 
 
 
@@ -40,5 +73,7 @@ const getAddressID = async(req, res) => {
 
 module.exports = {
     getAllAddress, 
-    getAddressID
+    getAddressID,
+    getAllDrivers,
+    getDriverID
 };
--- server/routes/api.js
+++ server/routes/api.js
@@ -8,4 +8,8 @@ router.get("/api/address", apiController.getAllAddress);
 
 router.get("/api/address/:id", apiController.getAddressID);
 
+router.get("/api/drivers", apiController.getAllDrivers);
+
+router.get("/api/drivers/:id", apiController.getDriverID);
+
 module.exports = router;
