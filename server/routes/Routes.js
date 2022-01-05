85d38f0ea16ec5ed1256ecde8a4b9112ef10f1f4
J-Hawes <91867589+J-Hawes@users.noreply.github.com>
2022-01-05 11:10:53 +1100

    db insert files

--- server/routes/Routes.js
+++ server/routes/Routes.js
@@ -8,10 +8,5 @@ router.get("/api/address", apiController.getAllAddress);
 
 router.get("/api/address/:id", apiController.getAddressID);
 
-router.get("/api/drivers", apiController.getAllDrivers);
-
-router.get("/api/drivers/:id", apiController.getDriverID);
-
-router.post("/api/drivers/#", apiController.insertDriver);
 
 module.exports = router;
