ea537d9296bb097c51575eea614bdcc79d62faeb
J-Hawes <91867589+J-Hawes@users.noreply.github.com>
2022-01-15 18:40:33 +1100

    updated sort

--- client/src/components/Footer/FooterScrubBar.js
+++ client/src/components/Footer/FooterScrubBar.js
@@ -91,8 +91,6 @@ export default function FooterScrubBar({ play, historyMode, action, setDirection
   //Setting state to use event changes and set new value when slider is moved
   const [value, setValue] = React.useState(getTimeAsMinutes(new Date())); 
 
-
-
   const handleChange = (event, newValue) => {
     setValue(newValue);
     timeCallback(scrubTimer(newValue));
@@ -148,9 +146,6 @@ export default function FooterScrubBar({ play, historyMode, action, setDirection
     }
 
   }, [historyMode, action, play, setDirection, timeCallback, value]);
-
-
- 
 
 
   return (
